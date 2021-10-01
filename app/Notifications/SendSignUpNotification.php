7c03ba44df1102a9cbe1cee733c0b1c28a136fd5
emmatblingx <macbook@EmmanuelTobi.local>
2021-10-01 05:35:29 +0100

    added SendSignUp Notification

---
+++ app/Notifications/SendSignUpNotification.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use App\User;
+
+class SendSignUpNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    protected $user;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->greeting("Hello Sharesquare Capsule CRM Sales Head")
+                    ->subject('New User Signup')
+                    ->line("A new user just registered succesfully with details shown below")
+                    ->line("Firstname: " . $this->user->name)
+                    ->line("Email:  " . $this->user->email)
+                    ->line('Thank you ');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
